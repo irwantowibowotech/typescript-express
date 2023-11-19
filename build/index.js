17eb5940ec201dda6f915587aa0a9e9dd9fed652
irwantowibowotech <66348834+irwantowibowotech@users.noreply.github.com>
2023-11-19 11:07:57 +0700

    implement typescript (#1)

--- build/index.js
+++ build/index.js
@@ -4,14 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.route('/').get((req, res) => {
-    res.send({
-        success: true,
-        message: 'Berhasil mengakses route',
-    });
-});
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.routes();
+    }
+    routes() {
+        this.app.route('/').get((req, res) => {
+            res.status(200).json({
+                message: 'Success accessing route'
+            });
+        });
+    }
+}
 const port = 8000;
+const app = new App().app;
 app.listen(port, () => {
-    console.log(`Server berjalan di port ${port}`);
+    console.log(`Mantab, berjalan di port ${port}`);
 });
+// const app = express();
+// app.route('/').get((req, res) => {
+// 	res.send({
+// 		success: true,
+// 		message: 'Berhasil mengakses route',
+// 	});
+// });
+// const port = 8000;
+// app.listen(port, () => {
+// 	console.log(`Server berjalan di port ${port}`);
+// });
